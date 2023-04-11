3f2a3cbf3759cd9fc324737df3ceb38043be0d91
aansyawaludddin <aansyawaluddin22122003@gmail.com>
2023-04-11 23:52:33 +0800

    Kumpul tugas 4

---
+++ H071221031/Minggu 4/login_app/StringUtils.java
@@ -0,0 +1,36 @@
+package login_app.utils;
+
+public class StringUtils {
+    /*
+    TODO
+     * Buatlah sebuah method static yang akan
+     * mengembalikan nickName berdasarkan fullName yang diberikan
+     * aturan pembuatan nickName adalah :
+     * 1. Jika fullName hanya 1 kata maka nickName = fullName
+     * (ex: FullName = Agus, maka NickName = Agus)
+     * 2. Jika fullName lebih dari 1 kata maka nickName adalah kata kedua dari
+     * fullName
+     * (ex: FullName = Eurico Devon, maka NickName = Devon)
+     */
+    public static String generateNickname(String fullName) {
+        String[] splitName = fullName.split(" ");
+        if (splitName.length == 1) {
+            // Jika fullName hanya 1 kata maka nickName = fullName
+            return fullName;
+        } else {
+            // Jika fullName lebih dari 1 kata maka nickName adalah kata kedua dari fullName
+            return splitName[1];
+        }
+    }
+    /*
+    TODO
+     * Buatlah sebuah method static yang akan
+     * mengembalikan nickName berdasarkan fullName yang diberikan
+     * aturan pembuatan nickName adalah :
+     * 1. Jika fullName hanya 1 kata maka nickName = fullName
+     * (ex: FullName = Agus, maka NickName = Agus)
+     * 2. Jika fullName lebih dari 1 kata maka nickName adalah kata kedua dari
+     * fullName
+     * (ex: FullName = Eurico Devon, maka NickName = Devon)
+     */
+}
